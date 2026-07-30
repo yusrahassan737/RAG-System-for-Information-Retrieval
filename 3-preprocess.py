2c95bb1100682d9538bd73be1581889216b78c39
Yusra Hassan <77341222+yusrahassan737@users.noreply.github.com>
2026-07-30 18:07:38 -0400

    rename

--- 3-preprocess.py
+++
@@ -1,3 +0,0 @@
-# Date: 
-# Class: CP423
-# Description: Script to clean, tokenize, etc. individual reports
---
+++ 3-retrieve.py
@@ -0,0 +1,5 @@
+# Date: 
+# Class: CP423
+# Description: Script to perform BM25 and Dense Retrieval
+# Dense Retrieval: Use a pretrained embedding model (e.g., Word2Vec or Sentence Transformers), to generate document
+# and query embeddings and retrieve documents based on embedding similarity.
