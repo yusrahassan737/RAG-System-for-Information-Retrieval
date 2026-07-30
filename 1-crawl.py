ceaf5ff44b3f163c52ba50fceb255616d4102b20
Yusra <yusrahassan737@gmail.com>
2026-07-30 15:46:53 -0400

    attempt 1 at restoring changes

--- 1-crawl.py
+++ 1-crawl.py
@@ -7,11 +7,12 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import time
+import os
 
 # Access root page
 root_url = "https://tsb.gc.ca"
 main_url = root_url + "/eng/rapports-reports/rail/index.html"
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"} # apparently helps request get accepted
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"} # helps request get accepted by website
 time.sleep(2)
 root_page = requests.get(main_url, headers=headers)
 
@@ -21,18 +22,24 @@ table = soup.tbody
 
 # Extract all page links
 links = table.find_all("a", href=True)
-links_tidy = {}
-for i in links:
-    full_link = root_url + i.get("href").strip()
-    links_tidy[str(i.text)] = full_link
+doc_info = {"doc_id": [], "source": [], "investigation_num": []}
+for i in range(len(links)):
+    full_link = root_url + links[i].get("href").strip()
+    doc_info["doc_id"].append(i)
+    doc_info["source"].append(full_link)
+    doc_info["investigation_num"].append(str(links[i].text))
 
 # Visit each link and download page
-for investigation_num in links_tidy:
-    time.sleep(2)
-    report = requests.get(links_tidy[investigation_num], headers=headers)
-    with open("reports/" + investigation_num + ".txt", "w") as f:
-        f.write(report.text)
+for i in range(len(links)):
+    investigation_num = doc_info["investigation_num"][i]
+    if (investigation_num + ".html") not in os.listdir("reports"):
+        time.sleep(2)
+        report = requests.get(doc_info["source"][i], headers=headers)
+        with open("reports/" + investigation_num + ".txt", "w", encoding="utf-8") as f:
+            f.write(report.text)
 
 # Save links
-with open("page_contents.txt", "w") as f:
-    f.write(str(links_tidy))
+doc_info_df = pd.DataFrame(doc_info)
+doc_info_df.to_csv("doc_info.csv")
+
+print(f"There are {i} reports downloaded.")
--- 2-parse.py
+++ 2-parse.py
@@ -1,33 +1,22 @@
 # code to parse individual report
 from bs4 import BeautifulSoup
+import os
 
-with open("test.html", "r") as f:
-    text = f.read()
+all_reports = os.listdir("reports")
+for report in all_reports:
+    with open("reports/" + report, "r", encoding = "utf-8") as f:
+        text = f.read()
 
-soup = BeautifulSoup(text, "html.parser")
+    soup = BeautifulSoup(text, "html.parser")
 
+    add_to_file = [soup.find('meta', attrs={'name': 'dcterms.title'}),
+                soup.find('meta', attrs={'name': 'dcterms.creator'}),
+                soup.find('meta', attrs={'name': 'dcterms.modified'}),
+                soup.find('title'),
+                soup.find('article')]
+    open_file = open("test.txt", "w", encoding = "utf-8")
+    for i in add_to_file:
+        if i:
+            open_file.write(i.text + "\n")
 
-add_to_file = [soup.find('meta', attrs={'name': 'dcterms.title'}),
-            soup.find('meta', attrs={'name': 'dcterms.creator'}),
-            soup.find('meta', attrs={'name': 'dcterms.modified'}),
-            soup.find('title'),
-            soup.find('h1', attrs={'class': 'page-header'})]
-open_file = open("test.txt", "w")
-for i in add_to_file:
-    if i.text:
-        open_file.write(i.text + "\n")
-
-open_file.close()
-
-soup.find_all()
-
-
-# things to keep
-"""
-<meta name = dcterms.title
-dcterms.creator
-dcterms.modified
-<title>
-<h1 id="wb-cont" class="page-header mrgn-tp-md">Railway Investigation Report R95Q0019</h1>
-everything-ish inside of article
-"""
+    open_file.close()
