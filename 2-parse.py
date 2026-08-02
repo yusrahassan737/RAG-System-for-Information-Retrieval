335be5ffdb5760c03c021917d1cc050b633b48ea
Yusra <yusrahassan737@gmail.com>
2026-08-02 04:06:40 -0400

    added some logic for indxing and bm25

--- 2-parse.py
+++ 2-parse.py
@@ -20,6 +20,27 @@ def pre_process(text):
     # Clean, then tokenize. Next, remove stopwords and lemmatize.
     return text
 
+# Add index info for one doc
+def create_doc_index(id, doc_num, tokens):
+    # Save term and document stats
+    term_freqs = pd.Series(tokens).explode().value_counts()
+    term_freqs = term_freqs.reset_index(name = "term_freq")
+    term_freqs["doc_no"] = doc_num # assign the same doc_id to every token in this iteration of the loop
+    return term_freqs
+
+# Invert the complete index file
+def construct_inverted_index(ind):
+    # Sort postings
+    ind = ind.sort_values(by = ["term", 'doc_no'])
+    ind["doc_info"] = ind["doc_no"].astype(str) + " (" + ind["term_freq"].astype(str) + ")"
+
+    # Group by term, aggregate the strings of the ids together per term
+    inverted_ind = ind.groupby(by = 'term', as_index=False).agg({'doc_info': ', '.join})
+    inverted_ind["doc_freq"] = inverted_ind["doc_info"].str.count(",") + 1
+    inverted_ind["doc_freq"] = np.where(inverted_ind["doc_info"] != "", inverted_ind["doc_freq"], 0)
+
+    return inverted_ind
+
 for report in all_reports:
     # Read document
     with open("reports/" + report, "r", encoding = "utf-8") as f:
@@ -45,6 +66,18 @@ for report in all_reports:
     # Pre-process the text, then get term-doc information
     tokens = pre_process(text)
     doc_lengths.append(len(tokens))
+
+    term_frequencies = create_doc_index(report, num, tokens)
+
+#     # Add this document's terms to the overall index
+#     index = pd.concat([index, term_frequencies])
+
+# # Create inverted index
+# index = index.rename(columns={"index":"term"})
+# index.to_csv("index.csv")
+# inverted_index = construct_inverted_index(index)
+# print(f"There are {len(inverted_index)} terms in the vocabulary/inverted index")
+# pd.DataFrame(inverted_index).to_csv("inverted_index.csv")
 
 # Add metadata and document lengths to file
 for i in metadata: 
--- 3-retrieve.py
+++ 3-retrieve.py
@@ -1,5 +1,55 @@
-# Date: 
+# Date: August 1, 2026
 # Class: CP423
 # Description: Script to perform BM25 and Dense Retrieval
 # Dense Retrieval: Use a pretrained embedding model (e.g., Word2Vec or Sentence Transformers), to generate document
 # and query embeddings and retrieve documents based on embedding similarity.
+
+import pandas as pd
+import re
+import math
+
+# Compute BM25 for a given query for every document in matching postings lists
+def compute_bm25(query, inverted_index, dl_lookup): # query is a list of tokens
+    # Hyperparameters
+    k1 = 1.2
+    b = 0.75
+    k3 = 1000
+
+    # Variables
+    N = len(dl_lookup) # total docs in the collection
+    AVG_DL = sum(dl_lookup.values()) / N # average document length
+    bm25_scores = {}
+
+    # Look through one token of the query at a time
+    for i in query:
+        # Get query term frequencies
+        qtf = query.count(i)
+
+        # Get the postings list for each token in the query
+        line = inverted_index[inverted_index["term"] == i]
+
+        # Only compute score if the term exists in the collection
+        if len(line) != 0:
+            df = line["doc_freq"].item()
+            idf = math.log((N - df + 0.5) / (df + 0.5))
+            matching_docs = line["doc_info"].item().split(",")
+
+            # Score one document at a time
+            for j in matching_docs:
+                id, tf = re.search(r"(.+) \((\d+)\)", j).groups() # extract the doc id and tf
+                id = id.strip()
+                tf = int(tf)
+                dl = dl_lookup[id]
+
+                formula = idf * (((tf * (k1 + 1))
+                / (tf + k1 * (1 - b + b * (dl / AVG_DL))))
+                * (((k3 + 1) * qtf) / (k3 + qtf))
+                )
+
+                # Add score to previous if it exists
+                if id not in bm25_scores:
+                    bm25_scores[id] = formula
+                else:
+                    bm25_scores[id] += formula
+    df = pd.DataFrame(bm25_scores.items(), columns=["document", "score"])
+    return df
